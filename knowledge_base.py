22d4ee2eca41110813151da5e9623a1c481ca198
ANIL KUMAR DUBALA <anildubala10292@gmail.com>
2025-05-23 13:37:16 +0530

    Update knowledge_base.py with QdrantClient integration

--- knowledge_base.py
+++ knowledge_base.py
@@ -4,11 +4,14 @@ from langchain.llms import OpenAI
 
 class KnowledgeBase:
     def __init__(self):
-        self.client = QdrantClient(url="http://localhost:6333")  # Run qdrant locally or use cloud
+        # Replace your local URL and no API key with cloud URL + API key here:
+        self.client = QdrantClient(
+            url="https://19f142e4-5101-4604-b7ba-a0ab51c1af5a.eu-west-1-0.aws.cloud.qdrant.io",
+            api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.rs-8jlqocaYQMn0qyEK2O5iu27nqgW2n1NXEzDikgxg"
+        )
         self.index = self.load_index()
 
     def load_index(self):
-        # For demo: Load some math Q&A documents
         documents = SimpleDirectoryReader('math_qa_docs').load_data()
         llm_predictor = LLMPredictor(llm=OpenAI(temperature=0))
         service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
